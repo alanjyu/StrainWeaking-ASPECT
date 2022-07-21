faae16aab1f7a7eb47f1e6e616bfbcc81678cdd6
Alan Yu <alanjhyu@gmail.com>
2022-07-21 16:29:52 -0400

    Move viscous-plastic project to another repository

--- README.md
+++ README.md
@@ -14,99 +14,25 @@
 | --- | --- | --- | --- | --- | --- | --- | --- |
 | Base | heron_2022.prm | 30 | 20.e6 | 0.5 | 1.5 | 0.5 | 0.5 |
 | [Brune et al., 2013](https://www.sciencedirect.com/science/article/pii/S0040195113000899?via%3Dihub) | brune_2013.prm | 15 | 20.e6 | 0.05 | 1 | 0.5? | 0.5? |
-| [Allken et al., 2013*](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2012GC004077) | allken_2013.prm | 15 | 20.e6 | 0.0 | 1.25 | 0.25? | 0.25? |
-[Duclaux et al., 2019*](https://www.sciencedirect.com/science/article/pii/S0012821X19306442) | | 15-2 | 20.-4.e6 | 0.5 | 1.5 | 0.1–0.5? | 0.1–0.5? |
+| [Allken et al., 2012*](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2012GC004077) | allken_2012.prm | 15 | 20.e6 | 0.0 | 1.25 | 0.25? | 0.25? |
+| [Duclaux et al., 2019*](https://www.sciencedirect.com/science/article/pii/S0012821X19306442) | | 15-2 | 20.e6-4.e6 | 0.5 | 1.5 | 0.1–0.5? | 0.1–0.5? |
 | [Continental extension cookbook](https://github.com/geodynamics/aspect/blob/main/cookbooks/continental_extension/continental_extension.prm) | heron_2022_.25wf.prm| 30 | 20.e6 | 0.5 | 1.5 | 0.25 | 0.25
 | [Heron et al., 2019](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2019TC005578) | heron_2019.prm | 20 | 20.e6 | 0.0 | 0.5 | 0.5 | 0.5 |
-| [Naliboff et al., 2020](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2019GL086611) | heron_2022.prm | 30 | 20.e6 | 0.5 | 1.5 | 0.5 | 0.5 |
-| [Gouiza & Naliboff, 2021](https://www.nature.com/articles/s41467-021-24945-5) | gouiza_2021_viscous.prm | 30 | 20.e6 | 0.5 | 1.5 | 1.0 | 0.25
+| [Naliboff et al., 2020](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2019GL086611) | | 30 | 20.e6 | 0.5 | 1.5 | 0.5 | 0.5 |
+| [Gouiza & Naliboff, 2021](https://www.nature.com/articles/s41467-021-24945-5) | gouiza_2021.prm | 30 | 20.e6 | 0.5 | 1.5 | 1.0 | 0.25
 
 \* These sources might use different equations or code libraries.
 
 
-## List of modified codeblocks
-
-The following codeblocks have been modified to be compatible with the base parameter file.
+## List of original parameters
 
 - Base & Naliboff et al., 2020 [(original file)](https://github.com/naliboff/aspect/blob/naliboff_etal_2020_grl/naliboff_etal_2020_grl.prm)
-  ```
-  set Angles of internal friction = 30., 30., 30., 30., 30., 30., 0., 30.
-  set Cohesions = 20.e6
-  set Use strain weakening = true
-  set Use plastic strain weakening = true
-  set Start plasticity strain weakening intervals  = 0.5
-  set End plasticity strain weakening intervals    = 1.5
-  set Cohesion strain weakening factors            = 0.5
-  set Friction strain weakening factors            = 0.5
-  ```
-
-- Brune et al., 2013
-
-  ```
-  set Angles of internal friction = 15., 15., 15., 15., 15., 15., 0., 15.
-  set Cohesions = 20.e6
-  set Use strain weakening = true
-  set Use plastic strain weakening = true
-  set Start plasticity strain weakening intervals  = 0.05
-  set End plasticity strain weakening intervals    = 1.0
-  set Cohesion strain weakening factors            = 0.5
-  set Friction strain weakening factors            = 0.5
-  ```
-
-- Allken et al., 2013
-
-  ```
-  set Angles of internal friction = 15., 15., 15., 15., 15., 15., 0., 15.
-  set Cohesions = 20.e6
-  set Use strain weakening = true
-  set Use plastic strain weakening = true
-  set Start plasticity strain weakening intervals  = 0.0
-  set End plasticity strain weakening intervals    = 1.25
-  set Cohesion strain weakening factors            = 0.25
-  set Friction strain weakening factors            = 0.25
-  ```
 
 - Continental extension cookbook [(original file)](https://github.com/geodynamics/aspect/blob/main/cookbooks/continental_extension/continental_extension.prm)
 
-  ```
-  # Plasticity parameters
-  set Angles of internal friction = 30., 30., 30., 30., 30., 30., 0., 30.
-  set Cohesions = 20.e6
-  set Strain weakening mechanism = plastic weakening with plastic strain only
-  set Start plasticity strain weakening intervals  = 0.5
-  set End plasticity strain weakening intervals    = 1.5
-  set Cohesion strain weakening factors            = 0.25
-  set Friction strain weakening factors            = 0.25
-  ```
-
 - Heron et al., 2019 [(original file)](https://github.com/heronphi/HeronTectonics2019/blob/master/HeronTectonics_M1.prm)
 
-  ```
-  set Angles of internal friction = 20., 20., 20., 20., 20., 20., 0., 20.
-  set Cohesions = 20.e6
-  set Use strain weakening = true
-  set Start strain weakening intervals  = 0.0
-  set End strain weakening intervals    = 0.5
-  set Cohesion strain weakening factors = 0.5
-  set Friction strain weakening factors = 0.5
-  ```
-
-- Gouiza & Naliboff, 2021 [(original files)](https://github.com/naliboff/aspect/tree/labrador_sea_gouiza_naliboff_2020/labrador_sea_gouiza_naliboff_2020_models). Note that the models also considered viscous strain. This will be incorporated into the modified file later.
-
-  ```
-  set Angles of internal friction = 30., 30., 30., 30., 30., 30., 0., 30.
-  set Cohesions = 20.e6
-  set Use strain weakening                        = true
-  set Use viscous strain weakening                = true
-  set Start prefactor strain weakening intervals  = 0.5 
-  set End prefactor strain weakening intervals    = 1.5 
-  set Prefactor strain weakening factors          = 0.25 
-  set Use plastic strain weakening                = true
-  set Start plasticity strain weakening intervals = 0.5
-  set End plasticity strain weakening intervals   = 1.5
-  set Cohesion strain weakening factors           = 1.0
-  set Friction strain weakening factors           = 0.25
-  ```
+- Gouiza & Naliboff, 2021 [(original files)](https://github.com/naliboff/aspect/tree/labrador_sea_gouiza_naliboff_2020/labrador_sea_gouiza_naliboff_2020_models).
 
 ## To read
   
--- plot_max_top.py
+++ plot_max_top.py
@@ -1,27 +1,26 @@
 # plotting time vs maximum topography
 
+from re import A
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 # import stats file from line 29
-stats_h22 = open(r'C:\Users\alanj\Downloads\output-heron_2022\statistics').readlines()[28:] # heron_2022
+stats_h22 = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-heron_2022\statistics').readlines()[28:] # heron_2022
 
-stats_15aif = open(r'C:\Users\alanj\Downloads\output-heron_2022_15ang\statistics').readlines()[28:] # heron_2022_15aif
+stats_15aif = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-heron_2022_15aif\statistics').readlines()[28:] # heron_2022_15aif
 
-stats_10coh = open(r'C:\Users\alanj\Downloads\output-heron_2022_10coh\statistics').readlines()[28:] # heron_2022_10coh
+stats_10coh = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-heron_2022_10coh\statistics').readlines()[28:] # heron_2022_10coh
 
-stats_25wf = open(r'C:\Users\alanj\Downloads\output-heron_2022_halfwf\statistics').readlines()[28:] # heron_2022_.25wf
+stats_25wf = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-heron_2022_.25wf\statistics').readlines()[28:] # heron_2022_.25wf
 
-stats_h19 = open(r'C:\Users\alanj\Downloads\output-heron_2019\statistics').readlines()[28:] # heron_2019
+stats_h19 = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-heron_2019\statistics').readlines()[28:] # heron_2019
 
-stats_a13 = open(r'C:\Users\alanj\Downloads\output-allken_2013\statistics').readlines()[28:] # allken_2013
+stats_a13 = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-allken_2013\statistics').readlines()[28:] # allken_2013
 
-stats_b13 = open(r'C:\Users\alanj\Downloads\output-brune_2013\statistics').readlines()[28:] # brune_2013
+stats_b13 = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-brune_2013\statistics').readlines()[28:] # brune_2013
 
-stats_g21 = open(r'C:\Users\alanj\Downloads\output-gouiza_2021\statistics').readlines()[28:] # gouiza_2021
-
-stats_g21v = open(r'C:\Users\alanj\Downloads\output-gouiza_2021_viscous\statistics').readlines()[29:] # gouiza_2021_viscous
+stats_g21 = open(r'C:\Users\alanj\OneDrive - University of Toronto\Slivers\output-gouiza_2021\statistics').readlines()[28:] # gouiza_2021
 
 
 # append each line into a stripped list
@@ -57,10 +56,6 @@ r_g21 = []
 for s in stats_g21:
   r_g21.append(s.strip().split())
 
-r_g21v = []
-for s in stats_g21v:
-  r_g21v.append(s.strip().split())
-
 
 # time in yr
 t_h22 = np.zeros(len(r_h22))
@@ -71,94 +66,85 @@ t_h19 = np.zeros(len(r_h19))
 t_a13 = np.zeros(len(r_a13))
 t_b13 = np.zeros(len(r_b13))
 t_g21 = np.zeros(len(r_g21))
-t_g21v = np.zeros(len(r_g21v))
 
 
 # max topography in km
 mt_h22 = np.zeros(len(r_h22))
 for i in range(len(r_h22)):
-  t_h22[i] = float(r_h22[i][1])/1e7 # convert yr to myr
+  t_h22[i] = float(r_h22[i][1])/1e6 # convert yr to myr
   mt_h22[i] = float(r_h22[i][22])/1e3 # convert m to km
 
 mt_15aif = np.zeros(len(r_15aif))
 for i in range(len(r_15aif)):
-  t_15aif[i] = float(r_15aif[i][1])/1e7
+  t_15aif[i] = float(r_15aif[i][1])/1e6
   mt_15aif[i] = float(r_15aif[i][22])/1e3
 
 mt_10coh = np.zeros(len(r_10coh))
 for i in range(len(r_10coh)):
-  t_10coh[i] = float(r_10coh[i][1])/1e7
+  t_10coh[i] = float(r_10coh[i][1])/1e6
   mt_10coh[i] = float(r_10coh[i][22])/1e3
 
 mt_25wf = np.zeros(len(r_25wf))
 for i in range(len(r_25wf)):
-  t_25wf[i] = float(r_25wf[i][1])/1e7
+  t_25wf[i] = float(r_25wf[i][1])/1e6
   mt_25wf[i] = float(r_25wf[i][22])/1e3
 
 mt_h19 = np.zeros(len(r_h19))
 for i in range(len(r_h19)):
-  t_h19[i] = float(r_h19[i][1])/1e7
+  t_h19[i] = float(r_h19[i][1])/1e6
   mt_h19[i] = float(r_h19[i][22])/1e3
 
 mt_a13 = np.zeros(len(r_a13))
 for i in range(len(r_a13)):
-  t_a13[i] = float(r_a13[i][1])/1e7
+  t_a13[i] = float(r_a13[i][1])/1e6
   mt_a13[i] = float(r_a13[i][22])/1e3
 
 mt_b13 = np.zeros(len(r_b13))
 for i in range(len(r_b13)):
-  t_b13[i] = float(r_b13[i][1])/1e7
+  t_b13[i] = float(r_b13[i][1])/1e6
   mt_b13[i] = float(r_b13[i][22])/1e3
 
 mt_g21 = np.zeros(len(r_g21))
 for i in range(len(r_g21)):
-  t_g21[i] = float(r_g21[i][1])/1e7
+  t_g21[i] = float(r_g21[i][1])/1e6
   mt_g21[i] = float(r_g21[i][22])/1e3
 
-mt_g21v = np.zeros(len(r_g21v))
-for i in range(len(r_g21v)):
-  t_g21v[i] = float(r_g21v[i][1])/1e7
-  mt_g21v[i] = float(r_g21v[i][23])/1e3
 
-
-# plt.plot(t_h22, mt_h22, label='EXP-1')
-# plt.plot(t_15aif, mt_15aif, label='EXP-9')
-# plt.plot(t_10coh, mt_10coh, label='EXP-10')
-# plt.plot(t_25wf, mt_25wf, label='EXP-11')
-# plt.plot(t_h19, mt_h19, label='EXP-12')
-# plt.plot(t_a13, mt_a13, label='EXP-13')
-# plt.plot(t_b13, mt_b13, label='EXP-14')
-# plt.plot(t_g21, mt_g21, label='EXP-15')
-
-fig, ax = plt.subplots(1, 2, figsize=(12.5, 6))
+# generate figure sized 12.5'' x 6''
+fig, ax = plt.subplots(1, 2, figsize=(14, 6))
 
 
 # first subplot
 ax[0].plot(t_h22, mt_h22, label='EXP-1', color='blue')
-ax[0].plot(t_h19, mt_h19, label='EXP-12', linestyle='--', color='black')
-ax[0].plot(t_a13, mt_a13, label='EXP-13', linestyle='--', color='gray')
-ax[0].plot(t_b13, mt_b13, label='EXP-14', linestyle='--', color='lightgray')
+ax[0].plot(t_h19, mt_h19, label='EXP-20', linestyle='--', color='black')
+ax[0].plot(t_a13, mt_a13, label='EXP-21', linestyle='--', color='gray')
+ax[0].plot(t_b13, mt_b13, label='EXP-22', linestyle='--', color='lightgray')
 
-ax[0].set_xlim([0, 2.5])
+ax[0].set_xlim([0, 25])
 ax[0].set_ylim([0, 6])
-ax[0].set_xlabel('Time (m.y.)', fontweight="bold")
-ax[0].set_ylabel('Maximum Topography (km)', fontweight="bold")
+ax[0].tick_params(axis='x', labelsize=14)
+ax[0].tick_params(axis='y', labelsize=14)
+ax[0].set_xlabel('Time (m.y.)', fontweight='bold', fontsize=14)
+ax[0].set_ylabel('Maximum Topography (km)', fontweight='bold', fontsize=14)
 ax[0].grid(linestyle='dotted')
-ax[0].legend(loc="upper right")
+ax[0].legend(loc="upper right", fontsize=14)
 
 
 # second subplot
 ax[1].plot(t_h22, mt_h22, label='EXP-1', color='blue')
-ax[1].plot(t_15aif, mt_15aif, label='EXP-9', linestyle='-.', color='darkred')
-ax[1].plot(t_10coh, mt_10coh, label='EXP-10', linestyle=':', color='turquoise')
-ax[1].plot(t_25wf, mt_25wf, label='EXP-11', linestyle='--', color='red')
-ax[1].plot(t_g21, mt_g21, label='EXP-15')
+ax[1].plot(t_15aif, mt_15aif, label='EXP-17', linestyle='-.', color='darkred')
+ax[1].plot(t_10coh, mt_10coh, label='EXP-18', linestyle=':', color='turquoise')
+ax[1].plot(t_25wf, mt_25wf, label='EXP-19', linestyle='--', color='red')
+ax[1].plot(t_g21, mt_g21, label='EXP-23')
 
-ax[1].set_xlim([0, 2.5])
+ax[1].set_xlim([0, 25])
 ax[1].set_ylim([0, 6])
-ax[1].set_xlabel('Time (m.y.)', fontweight="bold")
+ax[1].tick_params(axis='x', labelsize=14)
+ax[1].tick_params(axis='y', labelsize=14)
+ax[1].set_xlabel('Time (m.y.)', fontweight='bold', fontsize=14)
+ax[1].set_ylabel('Maximum Topography (km)', fontweight='bold', fontsize=14)
 ax[1].grid(linestyle='dotted')
-ax[1].legend(loc="upper right")
+ax[1].legend(loc="upper right", fontsize=14)
 
 
 # show figure
